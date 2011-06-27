06f91151da8d8a67ca59a1897791ff6571445dc3
Erik Stainsby <erik.stainsby@roaringsky.ca>
2011-06-27 06:47:34 -0700

    With working window, drawing color blocks

---
+++ RSLightningTests/RSLightningTests.h
@@ -0,0 +1,17 @@
+//
+//  RSLightningTests.h
+//  RSLightningTests
+//
+//  Created by Erik Stainsby on 11-06-24.
+//  Copyright 2011 Roaring Sky Software (Canada). All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface RSLightningTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ RSLightningTests/RSLightningTests.m
@@ -0,0 +1,33 @@
+//
+//  RSLightningTests.m
+//  RSLightningTests
+//
+//  Created by Erik Stainsby on 11-06-24.
+//  Copyright 2011 Roaring Sky Software (Canada). All rights reserved.
+//
+
+#import "RSLightningTests.h"
+
+
+@implementation RSLightningTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in RSLightningTests");
+}
+
+@end
